aa29c946c9ed6c06f42efaeebed588a04a3dc420
thuongsam <86760654+thuongsam@users.noreply.github.com>
2021-07-01 16:16:35 +0700

    https://github.com/thuongsam/tang-crush

--- config.js
+++ config.js
@@ -1,9 +1,8 @@
-const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+
+CONFIG = {
+    introTitle: 'Anh  à!',
+    introDesc: `Em lười vẫn động quá 
+Nhưng lại rất muốn chạy trong tâm trí anh ,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
@@ -12,8 +11,7 @@ const CONFIG = {
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    mess: 'Em  biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc:Nhà không khá giảm nhưng vẫn muốn được gả cho anh .',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
-}
+    messLink: 'http://fb.com' // m.me//thuongsam
